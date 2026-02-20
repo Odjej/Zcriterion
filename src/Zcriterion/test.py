f9a8c3ff1b8a1c08d0500de4febb6e9cf3015645
loeedberg <loeirisbo@gmail.com>
2026-02-20 09:36:01 +0100

    Update test.py

--- src/Zcriterion/test.py
+++ src/Zcriterion/test.py
@@ -176,3 +176,4 @@ print("Max difference (gamma vs series):", np.nanmax(np.abs(difference_ana)))
 print("Max difference (series vs numerical):", np.nanmax(np.abs(difference_ser_num)))
 print("Max difference (gamma vs numerical):", np.nanmax(np.abs(difference_gam_num)))
 
+# yo
